da6a63523cd0ca27c4038b90b99e052cd6605961
keer2345 <keer2345@gmail.com>
2018-09-20 22:35:42 +0800

    flask_migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,26 @@
 import os
-from config import config
 
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+from config import config
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 db = SQLAlchemy()
 
 
 def create_app(config_enviroment):
     app = Flask(__name__)
+
     app.config.from_object(config[config_enviroment])
+
+    db.init_app(app)
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # not using sqlalchemy event system, hence disabling it
-    
+
     config[config_enviroment].init_app(app)
 
     return app
-
--- manager_script.py
+++ manager_script.py
@@ -1,20 +1,33 @@
 import os
 
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Shell
 
-from app import create_app
+from app import create_app, db
 from config import Config
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager  =Manager(app)
+manager = Manager(app)
+migrate = Migrate(app, db)
+
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, db=db)
 
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+# @manager.command
+# def recreate_db():
+#     """
+#     Recreates a local database. You probably should not use this on
+#     production.
+#     """
+#     db.drop_all()
+#     db.create_all()
+#     db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
-
