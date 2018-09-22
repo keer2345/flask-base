db88c792eabfdf885819aa7248b32e97b8e8eb7d
keer2345 <keer2345@gmail.com>
2018-09-22 22:12:36 +0800

    account/login.html

---
+++ app/account/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import Form
+from wtforms.fields import (BooleanField, PasswordField, StringField,
+                            SubmitField)
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import Email, EqualTo, InputRequired, Length
+
+
+class LoginForm(Form):
+    email = EmailField(
+        'Email',
+        validators=[InputRequired(), Length(1, 64),
+                    Email()],
+        description="Enter your email address")
+    password = PasswordField('Password', validators=[InputRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log in')
--- app/account/views.py
+++ app/account/views.py
@@ -1,9 +1,21 @@
-from flask import Blueprint, redirect, render_template
+from flask import Blueprint, flash, redirect, render_template
 
-account_bp= Blueprint('account', __name__)
+from app.account.forms import LoginForm
+from app.models import User
+
+account_bp = Blueprint('account', __name__)
 
 
-@account_bp.route('/login', methods=['GET'])
+@account_bp.route('/login', methods=['GET', 'POST'])
 def login():
     """Log in an existing user."""
-    return render_template('account/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if (user is not None and user.password_hash is not None
+                and user.verify_password(form.password.data)):
+            flash('You are now logged in. Welcome back!', 'success')
+        else:
+            flash('Invalid email or password.', 'form-error')
+
+    return render_template('account/login.html', form=form)
